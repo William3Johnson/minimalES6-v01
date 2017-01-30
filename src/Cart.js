94b91035117e4773ed38174a7e9956f4f1760d06
KJ-B <donkjb@Kevins-MacBook-Pro.local>
2017-01-30 09:55:28 -0500

    yes
    ...

---
+++ src/Cart.js
@@ -0,0 +1,7 @@
+export default class Cart	
+
+	Constructor(addToCart, delete, updateQty, price, sku, total, clear){
+		if (addToCart = "undefined", )
+		
+	}
+
--- src/index.js
+++ src/index.js
@@ -5,20 +5,29 @@
 // its like a launch pad, where you bring all your other classes
 // together for use.
 
-import Person from './Person';
-import Car from './Car';
-import Plane from './Plane'
+//import Car from './Car';
+//
+//let edward = new Person("Edward");
+//let eds_car = new Car("ford");
 
-let edward = new Person("Edward");
-let eds_car = new Car("ford");
+//edward.car = eds_car;
+//edward.car.drive(edward);
 
-edward.car = eds_car;
-edward.car.drive(edward);
+//let cessna = new Plane ("Cessna", "Boeing", 1972);
+//cessna.fly(thePlane);
 
-let cessna = new Plane ("Cessna", "Boeing", 1972);
-cessna.fly(thePlane);
-
-let thePlane = newPlane;
-newPlane.fly(thePlane);
+//let thePlane = newPlane;
+//newPlane.fly(thePlane);
 
 //new plane = cessna,Boeing, 1972
+
+import BestBuyWebService from './BestBuyWebService';
+
+let bbws = new BestBuyWebService();
+bbws.apiKey ="8ccddf4rtjz5k5btqam84qak";
+bbws.url = "http://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=" 
++ bbws.apiKey + "&format=json";
+
+bbws.getData();
+
+
